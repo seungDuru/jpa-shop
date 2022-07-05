bed5e245f7216712ac04ea9b6357a55d51131d23
seungduru <dlatmddn468@gmail.com>
2022-07-05 09:29:00 +0900

    member 관련 Entity, Repository 생성

---
+++ src/main/java/com/japbook/jpashop/domain/Member.java
@@ -0,0 +1,31 @@
+package com.japbook.jpashop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+import java.util.ArrayList;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Member {
+    @Id
+    @GeneratedValue
+    @Column(name = "member_id")
+    private Long id;
+
+    private String name;
+
+    @Embedded
+    private Address address;
+
+    @OneToMany(mappedBy = "member")
+    private List<Order> orders = new ArrayList<>();
+
+}
