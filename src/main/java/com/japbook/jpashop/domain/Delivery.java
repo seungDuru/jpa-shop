ad0700c828bce620f43e3b8bdd5128d14cf9125a
seungduru <dlatmddn468@gmail.com>
2022-07-18 13:12:59 +0900

    Delivery 관련 생성

---
+++ src/main/java/com/japbook/jpashop/domain/Delivery.java
@@ -0,0 +1,30 @@
+package com.japbook.jpashop.domain;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import javax.persistence.*;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@Entity
+public class Delivery {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "delivery_id")
+    private Long id;
+
+    @OneToOne(mappedBy = "delivery")
+    private Order order;
+
+    @Embedded
+    private Address address;
+
+    @Enumerated(EnumType.STRING)
+    private DeliveryStatus status; //READY, COMP
+}
