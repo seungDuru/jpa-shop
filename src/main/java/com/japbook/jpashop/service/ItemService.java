fea54ae0ff5ec753aa5232bea030b391db8da63f
seungduru <dlatmddn468@gmail.com>
2022-07-21 13:15:22 +0900

    create ItemController, ItemService

---
+++ src/main/java/com/japbook/jpashop/service/ItemService.java
@@ -0,0 +1,52 @@
+package com.japbook.jpashop.service;
+
+import com.japbook.jpashop.domain.item.Book;
+import com.japbook.jpashop.domain.item.Item;
+import com.japbook.jpashop.repository.ItemRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+
+@RequiredArgsConstructor
+@Service
+public class ItemService {
+
+    private final ItemRepository itemRepository;
+
+    @Transactional
+    public void saveItem(Item item) {
+        itemRepository.save(item);
+    }
+
+    @Transactional
+    public Item updateItem(Long itemId, Book param) {
+        Item findItem = itemRepository.findOne(itemId);
+        findItem.setPrice(param.getPrice());
+        findItem.setName(param.getName());
+        findItem.setStockQuantity(param.getStockQuantity());
+        return findItem;
+    }
+
+    /**
+     * 영속성 컨텍스트가 자동 변경
+     */
+    @Transactional
+    public void updateItem(Long id, String name, int price) {
+        Item item = itemRepository.findOne(id);
+        item.setName(name);
+        item.setPrice(price);
+    }
+
+    @Transactional(readOnly = true)
+    public List<Item> findItems() {
+        return itemRepository.findAll();
+    }
+
+    @Transactional(readOnly = true)
+    public Item findOne(Long itemId) {
+        return itemRepository.findOne(itemId);
+    }
+}
