cd681ebbdea0d25b5bac5c54ca361af9bb1a075a
seungduru <dlatmddn468@gmail.com>
2022-07-23 14:54:18 +0900

    ItemList 뷰 생성

---
+++ src/test/java/com/japbook/jpashop/service/ItemUpdateTest.java
@@ -0,0 +1,25 @@
+package com.japbook.jpashop.service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+
+import javax.persistence.EntityManager;
+import javax.persistence.PersistenceContext;
+
+@SpringBootTest
+public class ItemUpdateTest {
+
+    @PersistenceContext
+    EntityManager em;
+
+    @Test
+    public void updateTest() throws Exception{
+        //given
+
+        //when
+
+        //then
+
+    }
+}
